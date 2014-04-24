07b2f45dec267b7a75495698fe99bacb4b736302
Brendan Nee <brendan@blinktag.com>
2014-04-23 17:55:49 -0700

    Handle TOKEN passed in as parameter for testing

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 exports.index = function(req, res){
+  if(process.env.TOKEN) {
+    req.session.access_token = process.env.TOKEN;
+  }
+
   if(req.session && req.session.access_token) {
     res.render('app', {loggedIn: true});
   } else {
     res.render('index');
   }
 }
+
 
 exports.force_https = function(req, res, next) {
   if(req.headers['x-forwarded-proto'] != 'https') {
